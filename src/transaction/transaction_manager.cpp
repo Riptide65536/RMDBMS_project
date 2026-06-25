9637dfb575a88d76b064679e6be4d3b44aa4cc50
20161314 <1687699860@qq.com>
2026-06-25 12:59:22 +0800

    完成 Task09 显式事务提交与回滚

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -9,10 +9,41 @@ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
 #include "transaction_manager.h"
+#include "execution/executor_utils.h"
 #include "record/rm_file_handle.h"
 #include "system/sm_manager.h"
 
 std::unordered_map<txn_id_t, Transaction *> TransactionManager::txn_map = {};
+
+namespace {
+
+void delete_index_entries(SmManager *sm_manager, const TabMeta &tab, const std::string &table_name,
+                          const RmRecord &record, Transaction *txn) {
+    for (const auto &index : tab.indexes) {
+        auto index_name = sm_manager->get_ix_manager()->get_index_name(table_name, index.cols);
+        auto ih_it = sm_manager->ihs_.find(index_name);
+        if (ih_it == sm_manager->ihs_.end()) {
+            continue;
+        }
+        auto key = build_index_key(index, record);
+        ih_it->second->delete_entry(key.data(), txn);
+    }
+}
+
+void insert_index_entries(SmManager *sm_manager, const TabMeta &tab, const std::string &table_name,
+                          const RmRecord &record, const Rid &rid, Transaction *txn) {
+    for (const auto &index : tab.indexes) {
+        auto index_name = sm_manager->get_ix_manager()->get_index_name(table_name, index.cols);
+        auto ih_it = sm_manager->ihs_.find(index_name);
+        if (ih_it == sm_manager->ihs_.end()) {
+            continue;
+        }
+        auto key = build_index_key(index, record);
+        ih_it->second->insert_entry(key.data(), rid, txn);
+    }
+}
+
+}  // namespace
 
 /**
  * @description: 事务的开始方法
@@ -40,6 +71,9 @@ Transaction * TransactionManager::begin(Transaction* txn, LogManager* log_manage
  */
 void TransactionManager::commit(Transaction* txn, LogManager* log_manager) {
     if (txn == nullptr) {
+        return;
+    }
+    if (txn->get_state() == TransactionState::COMMITTED || txn->get_state() == TransactionState::ABORTED) {
         return;
     }
 
@@ -71,21 +105,48 @@ void TransactionManager::abort(Transaction * txn, LogManager *log_manager) {
     if (txn == nullptr) {
         return;
     }
+    if (txn->get_state() == TransactionState::COMMITTED || txn->get_state() == TransactionState::ABORTED) {
+        return;
+    }
 
     auto write_set = txn->get_write_set();
     while (!write_set->empty()) {
         auto *write_record = write_set->back();
         write_set->pop_back();
-        auto fh = sm_manager_->fhs_.at(write_record->GetTableName()).get();
+        const std::string &table_name = write_record->GetTableName();
+        auto &tab = sm_manager_->db_.get_table(table_name);
+        auto fh = sm_manager_->fhs_.at(table_name).get();
         switch (write_record->GetWriteType()) {
             case WType::INSERT_TUPLE:
-                fh->delete_record(write_record->GetRid(), nullptr);
+                if (fh->is_record(write_record->GetRid())) {
+                    auto record = fh->get_record(write_record->GetRid(), nullptr);
+                    delete_index_entries(sm_manager_, tab, table_name, *record, txn);
+                    fh->delete_record(write_record->GetRid(), nullptr);
+                }
                 break;
             case WType::DELETE_TUPLE:
                 fh->insert_record(write_record->GetRid(), write_record->GetRecord().data);
+                insert_index_entries(sm_manager_, tab, table_name, write_record->GetRecord(), write_record->GetRid(),
+                                     txn);
                 break;
             case WType::UPDATE_TUPLE:
-                fh->update_record(write_record->GetRid(), write_record->GetRecord().data, nullptr);
+                if (fh->is_record(write_record->GetRid())) {
+                    auto current_record = fh->get_record(write_record->GetRid(), nullptr);
+                    for (const auto &index : tab.indexes) {
+                        auto old_key = build_index_key(index, write_record->GetRecord());
+                        auto current_key = build_index_key(index, *current_record);
+                        if (memcmp(old_key.data(), current_key.data(), index.col_tot_len) != 0) {
+                            auto index_name = sm_manager_->get_ix_manager()->get_index_name(table_name, index.cols);
+                            auto ih_it = sm_manager_->ihs_.find(index_name);
+                            if (ih_it == sm_manager_->ihs_.end()) {
+                                continue;
+                            }
+                            ih_it->second->delete_entry(current_key.data(), txn);
+                            ih_it->second->insert_entry(old_key.data(), write_record->GetRid(), txn);
+                        }
+                    }
+                    fh->update_record(write_record->GetRid(), write_record->GetRecord().data, nullptr);
+                }
                 break;
             default:
                 break;
